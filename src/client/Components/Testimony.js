78dfa6b4450daddc6baa789e509462490ea89eb7
illestrater <illestrater@gmail.com>
2021-02-19 07:33:49 -0800

    Add bill elis testimony

--- src/client/Components/Testimony.js
+++ src/client/Components/Testimony.js
@@ -19,13 +19,15 @@ export default function Committee() {
       </div>
       <div className='page-container'>
         <div className='text-s margin-top'>
+          "The NFT space has changed my life. I feel like an artist with a clear mission. I am inspired and honoured to be in this space.
+          But most of all I am grateful for this initiative of change. We, as artists can help shape the future of this community and support upcoming creatives with
+          knowledge and support resulting in more inclusivity and talent in the NFT world"
+          -  <span className='text-s text-grey pointer' onClick={ () => openLink('https://twitter.com/billelis') }>Billelis</span>
+        </div>
+        <div className='text-s margin-top'>
           "I would never have imagined connecting with people who I consider legends. Now I have the opportunity to
           be part of a community of the most creative and forward thinking individuals in the world, and it drives me to do something great for others."
           -  <span className='text-s text-grey pointer' onClick={ () => openLink('https://twitter.com/illestrater_') }>illestrater</span>
-        </div>
-        <div className='text-s margin-top'>
-          "---------------------"
-          -  <span className='text-s text-grey pointer' onClick={ () => openLink('') }>Artist</span>
         </div>
         <div className='text-s margin-top'>
           "---------------------"
