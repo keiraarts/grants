8f47ff2fd70fbd60004e0fc41f855a45ab99f57e
illestrater <illestrater@gmail.com>
2021-02-19 06:13:54 -0800

    Add resources

--- src/client/Components/Resources.js
+++ src/client/Components/Resources.js
@@ -20,6 +20,8 @@ export default function Committee() {
       </div>
       <div className='page-container text-m'>
         <a className='text-grey pointer margin-top' onClick={ () => openLink('https://www.youtube.com/watch?v=a8ww4aNlPQU') }>Mankind's "What is an NFT?"</a><br />
+        <a className='text-grey pointer margin-top' onClick={ () => openLink('https://www.loop-news.com/p/beginners-guide-crypto-art-and-nfts') }>Loopify's Beginners Guide</a><br />
+        <a className='text-grey pointer margin-top' onClick={ () => openLink('https://coopahtroopa.mirror.xyz/PF42Z9oE_r6yhZN9jZrrseXfHaZALj9JIfMplshlgQ0') }>Coopahtroopa's NFT Landscape</a><br />
         <Link to='/tutorial' className='text-grey remove-a margin-top'>Setting up a wallet</Link><br />
         <Link to='/opensea' className='text-grey remove-a margin-top'>Minting on OpenSea</Link><br />
         <Link to='/rarible' className='text-grey remove-a margin-top'>Minting on Rarible</Link><br />
