1dac77006d38c4de6bc6238916f67342d7b7228f
Tim Kang <illestrater@gmail.com>
2021-02-21 21:15:36 -0800

    Express.json limit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-app.use(express.limit('100mb'));
+app.use(express.json({ limit: '100mb' }));
 
 const ENV = process.env;
 
