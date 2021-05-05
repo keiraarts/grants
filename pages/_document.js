0c9c66c1976fb98d3a8cb5b292486ea9d11d8bd8
keiraarts <32268475+keiraarts@users.noreply.github.com>
2021-05-04 21:15:15 -0400

    cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -47,7 +47,7 @@ class MyDocument extends Document {
           />
 
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff"></meta>
+          <meta name="theme-color" content="#FFF"></meta>
         </Head>
         <body>
           <Main />
