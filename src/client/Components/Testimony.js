1e4dd4199846d696a6088b4d4583821cdde46cc6
keiraarts <32268475+keiraarts@users.noreply.github.com>
2021-07-17 16:05:45 -0400

    cleanup

--- src/client/Components/Testimony.js
+++ src/client/Components/Testimony.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Committee() {
+export default function Testimony() {
   return (
     <div className="content-block">
       <div className="text-l text-b">
