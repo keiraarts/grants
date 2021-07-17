47b3224f75911580137dbd679254b12fb9eef077
keiraarts <32268475+keiraarts@users.noreply.github.com>
2021-07-17 14:35:29 -0400

    cleanup

--- pages/testimony.js
+++ pages/testimony.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Testimony from "../src/client/Components/Testimony";
+import Testimony from "../src/client/components/testimony";
 
 export default function TestimonyPage() {
   return <Testimony />;
